9284c3e7128de614bf849957f4933f0514f3c960
BigDany1792 <dany1792dota@gmail.com>
2022-07-21 23:46:22 -0500

    ci: added function that finds the uncle of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: is a pointer to the node to find the sibling
+ *
+ * Return: a pointer to the sibling node
+ *         If node is NULL or the parent is NULL, return NULL
+ *         If node has no sibling, return NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
+}
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: is a pointer to the node to find the uncle
+ *
+ * Return: a pointer to the uncle node
+ *         If node is NULL, return NULL
+ *         If node has no uncle, return NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
