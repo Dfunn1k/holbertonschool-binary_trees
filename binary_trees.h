3c9124c8f2dd1df5972d20aac3a1dcaae3eb0fb5
Christian Varas <christianvaras.2020@gmail.com>
2022-07-22 00:25:06 -0500

    Adding function for task advanced

--- binary_trees.h
+++ binary_trees.h
@@ -52,6 +52,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* Advanced */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 
